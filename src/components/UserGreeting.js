b005ff280a057f91e206c69facec7400ec75fddf
Niraj Pandey <nirajpandey034@gmail.com>
2021-02-14 18:53:39 +0530

    use of [] instead of () in return statement in render method, it will act as array of different kinda elements

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -39,29 +39,31 @@ class UserGreeting extends Component {
         // }
 
         //2nd way using JS variable
-        let message
+        // let message
 
-        if(this.state.isLoggedIn){
-            message = <div>Welcome Niraj</div>
-        }
-        else{
-            message = <div>Welcome Guest</div>
-        }
+        // if(this.state.isLoggedIn){
+        //     message = <div>Welcome Niraj</div>
+        // }
+        // else{
+        //     message = <div>Welcome Guest</div>
+        // }
 
-        return (
-            <div>
-                      {message}
-                      <button onClick={this.changeState}>{this.state.btnName}</button>
-            </div>
+        // return (
+        //     <div>
+        //               {message}
+        //               <button onClick={this.changeState}>{this.state.btnName}</button>
+        //     </div>
       
-        )
+        // )
         
         //3rd way using ternary operator
-        // return (
-        //     this.state.isLoggedIn ? 
-        //     <div>Welcome Niraj</div> : 
-        //     <div>Welcome Guest</div> 
-        // )
+        return [
+            this.state.isLoggedIn ? 
+            <div>Welcome Niraj</div> : 
+            <div>Welcome Guest</div> ,
+
+            <button onClick={this.changeState}>{this.state.btnName}</button>
+        ]
 
         //4th way using short circuit technique, only for if u want to show if logged in and nothing when not logged in
         //return this.state.isLoggedIn && <div>Welcome Niraj</div>
