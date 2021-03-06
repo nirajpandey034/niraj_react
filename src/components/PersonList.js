58e0fc33e160ff246a227b3874e8f5f7339f00a3
Niraj Pandey <nirajpandey034@gmail.com>
2021-03-06 20:53:43 +0530

    advance list rendering

--- src/components/PersonList.js
+++ src/components/PersonList.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import Person from './Person'
 
 function PersonList() {
-    let Persons = ['Niraj','Kumar','Pandey']
-    let person = Persons.map(person => <h2>{person}</h2>)
+    const Persons = [
+        {
+            id:1,
+            name:'Niraj',
+            age:23
+        },
+        {
+            id:2,
+            name:'Rahul',
+            age:27
+        },
+        {
+            id:3,
+            name:'Neha',
+            age:26
+        }
+    ]
+    //map function takes each element and applies the same method to all of them, like here calling the Person.js and returns an array
+    let persons  = Persons.map(person => <Person person = {person} />)
     return (
         <div>
-            {person}
+            {persons}
         </div>
     )
 }
